d49835e5c35dce39701d450d4de0c475fa3bc73e
ago <ago@localhost.localdomain>
2020-07-27 17:00:26 -0400

    maker and get user location

--- lib/Pages/Map.dart
+++ lib/Pages/Map.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import '../Provider/MapProvider.dart';
 import 'package:provider/provider.dart';
+
+import 'package:flutter/widgets.dart';
 
 class MapPage extends StatefulWidget {
   MapPage({Key key, this.title}) : super(key: key);
@@ -25,107 +28,166 @@ class Map extends StatefulWidget {
 }
 
 class _MapState extends State<Map> {
+  final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
+
+  Position _currentPosition;
+  String _currentAddress = '';
+
   @override
   Widget build(BuildContext context) {
     final mapState = Provider.of<MapState>(context);
     return new Scaffold(
       body: Stack(
-        children: <Widget>[
-          GoogleMap(
-            mapType: MapType.hybrid,
-            initialCameraPosition: mapState.kGooglePlex,
-            onMapCreated: (GoogleMapController controller) {
-              mapState.controller.complete(controller);
-            },
-          ),
-          Positioned(
-            top: 50.0,
-            right: 15.0,
-            left: 15.0,
-            child: Container(
-              height: 50.0,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(3.0),
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                      color: Colors.grey,
-                      offset: Offset(1.0, 5.0),
-                      blurRadius: 10,
-                      spreadRadius: 3)
-                ],
-              ),
-              child: TextField(
-                cursorColor: Colors.black,
-                // controller: appState.locationController,
-                decoration: InputDecoration(
-                  icon: Container(
-                    margin: EdgeInsets.only(left: 20, top: 5),
-                    width: 10,
-                    height: 10,
-                    child: Icon(
-                      Icons.my_location,
-                      color: Colors.blueAccent,
+              children: <Widget>[
+                // GoogleMap(
+                //   mapType: MapType.normal,
+                //   initialCameraPosition: mapState.kGooglePlex,
+                //   onMapCreated: (GoogleMapController controller) {
+                //     mapState.controller.complete(controller);
+                //   },
+                // ),
+                GoogleMap(
+                  initialCameraPosition: CameraPosition(
+                      target: LatLng(-4.0624, 39.6599), zoom: 10.0),
+                  onMapCreated: mapState.onCreated,
+                  myLocationEnabled: true,
+                  mapType: MapType.normal,
+                  compassEnabled: true,
+                  markers: mapState.markers,
+                  onCameraMove: mapState.onCameraMove,
+                  polylines: mapState.polyLines,
+                ),
+                Positioned(
+                  top: 50.0,
+                  right: 15.0,
+                  left: 15.0,
+                  child: Container(
+                    height: 50.0,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(3.0),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.grey,
+                            offset: Offset(1.0, 5.0),
+                            blurRadius: 10,
+                            spreadRadius: 3)
+                      ],
+                    ),
+                    child: TextField(
+                      cursorColor: Colors.black,
+                      controller: mapState.locationController,
+                      decoration: InputDecoration(
+                        icon: Container(
+                          margin: EdgeInsets.only(left: 20, top: 0),
+                          // width: 10,
+                          // height: 10,
+                          child: Icon(
+                            Icons.place,
+                            color: Colors.blueAccent,
+                          ),
+                        ),
+                        hintText: 'My Location',
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.only(left: 15.0, top: 16.0),
+                      ),
                     ),
                   ),
-                  hintText: "My Location",
-                  border: InputBorder.none,
-                  contentPadding: EdgeInsets.only(left: 15.0, top: 16.0),
                 ),
-              ),
-            ),
-          ),
-          Positioned(
-            top: 105.0,
-            right: 15.0,
-            left: 15.0,
-            child: Container(
-              height: 50.0,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(3.0),
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                      color: Colors.grey,
-                      offset: Offset(1.0, 5.0),
-                      blurRadius: 10,
-                      spreadRadius: 3)
-                ],
-              ),
-              child: TextField(
-                cursorColor: Colors.black,
-                // controller: appState.destinationController,
-                textInputAction: TextInputAction.go,
-                onSubmitted: (value) {
-                  // appState.sendRequest(value);
-                },
-                decoration: InputDecoration(
-                  icon: Container(
-                    margin: EdgeInsets.only(left: 20, top: 5),
-                    width: 10,
-                    height: 10,
-                    child: Icon(
-                      Icons.business,
-                      color: Colors.blueAccent,
+                Positioned(
+                  top: 105.0,
+                  right: 15.0,
+                  left: 15.0,
+                  child: Container(
+                    height: 50.0,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(3.0),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.grey,
+                            offset: Offset(1.0, 5.0),
+                            blurRadius: 10,
+                            spreadRadius: 3)
+                      ],
+                    ),
+                    child: TextField(
+                      cursorColor: Colors.black,
+                      controller: mapState.destinationController,
+                      textInputAction: TextInputAction.go,
+                      onSubmitted: (value) {
+                        mapState.sendRequest(value);
+                      },
+                      decoration: InputDecoration(
+                        icon: Container(
+                          margin: EdgeInsets.only(left: 20, top: 5),
+                          width: 10,
+                          height: 10,
+                          child: Icon(
+                            Icons.business,
+                            color: Colors.blueAccent,
+                          ),
+                        ),
+                        hintText: "Search Near Shop...",
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.only(left: 15.0, top: 16.0),
+                      ),
                     ),
                   ),
-                  hintText: "Search Near Shop...",
-                  border: InputBorder.none,
-                  contentPadding: EdgeInsets.only(left: 15.0, top: 16.0),
                 ),
-              ),
+              ],
             ),
-          ),
-        ],
-      ),
       floatingActionButton: FloatingActionButton(
         child: IconButton(
           icon: Icon(Icons.place),
-          onPressed: mapState.goToTheLake,
+          onPressed: () => _getCurrentLocation,
         ),
       ),
     );
+  }
+
+  @override
+  initState() {
+    setState(() {
+      super.initState();
+
+      _getCurrentLocation();
+      this._currentAddress = _currentAddress;
+    });
+  }
+
+  _getCurrentLocation() {
+    geolocator
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
+        .then((Position position) {
+      setState(() {
+        _currentPosition = position;
+      });
+      print('worlllllllllll');
+
+      _getAddressFromLatLng();
+    }).catchError((e) {
+      print(e);
+    });
+  }
+
+  _getAddressFromLatLng() async {
+    try {
+      List<Placemark> p = await geolocator.placemarkFromCoordinates(
+          _currentPosition.latitude, _currentPosition.longitude);
+
+      Placemark place = p[0];
+
+      setState(() {
+        _currentAddress =
+            "${place.locality}, ${place.postalCode}, ${place.country}";
+      });
+
+      print(1231212312442144234);
+    } catch (e) {
+      print(e);
+    }
   }
 }
--- lib/Provider/MapProvider.dart
+++ lib/Provider/MapProvider.dart
@@ -1,36 +1,200 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'dart:async';
 
+import 'MapRequests.dart';
+
 class MapState with ChangeNotifier {
-  static LatLng _initialPosition;
+//   static Position currentPosition;
+//   static LatLng _initialPosition;
+//   LatLng _lastPosition = _initialPosition;
+
+//   Completer<GoogleMapController> controller = Completer();
+  
+// // CameraPosition(target: LatLng(currentPosition.latitude, currentPosition.longitude));
+  
+//   final CameraPosition kGooglePlex = CameraPosition(
+//     target: LatLng(-0.8254137,34.6085512),
+//     // target: LatLng(currentPosition.latitude, currentPosition.longitude),
+//     zoom: 10,
+//   );
+
+//   final CameraPosition _kLake = CameraPosition(
+//     bearing: 192.8334901395799,
+//     target: LatLng(37.43296265331129, -122.08832357078792),
+//     tilt: 59.440717697143555,
+//     zoom: 19.151926040649414
+//   );
+
+//   MapState() {
+//     goToTheLake();
+//     getUserCurrentLocation();
+//     // _loadingInitialPosition();
+//   }
+//   void goToTheLake() async {
+
+//     // final GoogleMapController controller = await controller.Future;
+//     // controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+//     // notifyListeners();
+//   }
+
+//    void getUserCurrentLocation() {
+//     final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
+
+//     geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.best).then((Position position) {
+//         currentPosition = position;
+//         print("=============================  $currentPosition");
+//     }).catchError((e) {
+//       print(e);
+//     });
+
+//     notifyListeners();
+//   }
+//   // ! ON CREATE
+//   void onCreated(GoogleMapController controller) {
+//     _mapController = controller;
+//     notifyListeners();
+//   }
+static LatLng _initialPosition;
   LatLng _lastPosition = _initialPosition;
+  bool locationServiceActive = true;
 
-  Completer<GoogleMapController> controller = Completer();
+  final Set<Marker> _markers = {};
+  final Set<Polyline> _polyLines = {};
 
-  final CameraPosition kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
+  GoogleMapController _mapController;
+  GoogleMapsServices _googleMapsServices = GoogleMapsServices();
 
-  final CameraPosition _kLake = CameraPosition(
-    bearing: 192.8334901395799,
-    target: LatLng(37.43296265331129, -122.08832357078792),
-    tilt: 59.440717697143555,
-    zoom: 19.151926040649414
-  );
+  TextEditingController locationController = TextEditingController();
+  TextEditingController destinationController = TextEditingController();
+
+  LatLng get initialPosition => _initialPosition;
+  LatLng get lastPosition => _lastPosition;
+
+  GoogleMapsServices get googleMapsServices => _googleMapsServices;
+  GoogleMapController get mapController => _mapController;
+  
+  Set<Marker> get markers => _markers;
+  Set<Polyline> get polyLines => _polyLines;
 
   MapState() {
-    goToTheLake();
-
-    // _loadingInitialPosition();
+    _getUserLocation();
+    _loadingInitialPosition();
   }
-  void goToTheLake() async {
-
-    // final GoogleMapController controller = await controller.Future;
-    // controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
-    // notifyListeners();
+// ! TO GET THE USERS LOCATION
+  void _getUserLocation() async {
+    Position position = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    List<Placemark> placemark = await Geolocator()
+        .placemarkFromCoordinates(position.latitude, position.longitude);
+    _initialPosition = LatLng(position.latitude, position.longitude);
+    print("the latitude is: ${position.longitude} and th longitude is: ${position.longitude} ");
+    print("initial position is : ${_initialPosition.toString()}");
+    locationController.text = placemark[0].name;
+    notifyListeners();
   }
 
+  // ! TO CREATE ROUTE
+  void createRoute(String encondedPoly) {
+    _polyLines.add(Polyline(
+        polylineId: PolylineId(_lastPosition.toString()),
+        width: 10,
+        points: _convertToLatLng(_decodePoly(encondedPoly)),
+        color: Colors.black));
+    notifyListeners();
+  }
+
+  // ! ADD A MARKER ON THE MAO
+  void _addMarker(LatLng location, String address) {
+    _markers.add(Marker(
+        markerId: MarkerId(_lastPosition.toString()),
+        position: location,
+        infoWindow: InfoWindow(title: address, snippet: "go here"),
+        icon: BitmapDescriptor.defaultMarker));
+    notifyListeners();
+  }
+
+  // ! CREATE LAGLNG LIST
+  List<LatLng> _convertToLatLng(List points) {
+    List<LatLng> result = <LatLng>[];
+    for (int i = 0; i < points.length; i++) {
+      if (i % 2 != 0) {
+        result.add(LatLng(points[i - 1], points[i]));
+      }
+    }
+    return result;
+  }
+
+  // !DECODE POLY
+  List _decodePoly(String poly) {
+    var list = poly.codeUnits;
+    var lList = new List();
+    int index = 0;
+    int len = poly.length;
+    int c = 0;
+// repeating until all attributes are decoded
+    do {
+      var shift = 0;
+      int result = 0;
+
+      // for decoding value of one attribute
+      do {
+        c = list[index] - 63;
+        result |= (c & 0x1F) << (shift * 5);
+        index++;
+        shift++;
+      } while (c >= 32);
+      /* if value is negetive then bitwise not the value */
+      if (result & 1 == 1) {
+        result = ~result;
+      }
+      var result1 = (result >> 1) * 0.00001;
+      lList.add(result1);
+    } while (index < len);
+
+/*adding to previous value as done in encoding */
+    for (var i = 2; i < lList.length; i++) lList[i] += lList[i - 2];
+
+    print(lList.toString());
+
+    return lList;
+  }
+
+  // ! SEND REQUEST
+  void sendRequest(String intendedLocation) async {
+    List<Placemark> placemark =
+        await Geolocator().placemarkFromAddress(intendedLocation);
+    double latitude = placemark[0].position.latitude;
+    double longitude = placemark[0].position.longitude;
+    LatLng destination = LatLng(latitude, longitude);
+    _addMarker(destination, intendedLocation);
+    String route = await _googleMapsServices.getRouteCoordinates(
+        _initialPosition, destination);
+    createRoute(route);
+    notifyListeners();
+  }
+
+  // ! ON CAMERA MOVE
+  void onCameraMove(CameraPosition position) {
+    _lastPosition = position.target;
+    notifyListeners();
+  }
+
+  // ! ON CREATE
+  void onCreated(GoogleMapController controller) {
+    _mapController = controller;
+    notifyListeners();
+  }
+
+//  LOADING INITIAL POSITION
+  void _loadingInitialPosition()async{
+    await Future.delayed(Duration(seconds: 5)).then((v) {
+      if(_initialPosition == null){
+        locationServiceActive = false;
+        notifyListeners();
+      }
+    });
+  }
 }
